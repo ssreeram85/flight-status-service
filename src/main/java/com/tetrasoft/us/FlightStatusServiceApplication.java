33fd102dd8d168e3ff4320dab71b5d73493b2ad3
ssanepalli Sreeramulu Sanepalli <ssanepalli@tetrasoft.us>
2019-10-16 16:57:57 +0530

    inital commit

---
+++ src/main/java/com/tetrasoft/us/FlightStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tetrasoft.us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightStatusServiceApplication.class, args);
+	}
+
+}
